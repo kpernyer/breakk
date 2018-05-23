e13527df52533a71194f219043f380b05c4dc146
gilbertoBE <gilberto.batres-estrada@webstep.se>
2018-05-23 18:01:41 +0200

    multi gpu lstm has to be implemented with for-loops

--- code/DL_mxnet_symbol.py
+++ code/DL_mxnet_symbol.py
@@ -110,22 +110,36 @@ def train_lstm(train_iter, val_iter,
                hidden_units=[1200, 500, 75],
                num_outputs=1):
     net_lstm = mx.sym.Variable('data')
+    weight = mx.sym.Variable('weight', init=mx.init.Zero())
+    bias = mx.sym.Variable('bias', init=mx.init.Zero())
+#    rnn_h_init = mx.sym.Variable('LSTM_init_h')
+#    rnn_c_init = mx.sym.Variable('LSTM_init_c')
+#    rnn_params = mx.sym.Variable('LSTM_bias')
+
+    num_hidden = 20
+    num_lstm_layer = 1
+
 #    net = mx.sym.transpose(net, (1, 0, 2))
     # maybe use mx.sym.transpose(data, axes=(1, 0, 2)) ?? (time, batch, columns)
     net_lstm = mx.sym.RNN(
             net_lstm,
-            num_layers=1,
-            state_size=10,
+            num_layers=num_lstm_layer,
+            state_size=num_hidden,
             name='rnn_lstm1',
             mode='lstm',
+#            state=rnn_h_init,
+#            state_cell=rnn_c_init,
+            parameters=weight,
             p=0.4)
 
-    net_lstm = mx.sym.FullyConnected(net_lstm,
+    net_lstm = mx.sym.FullyConnected(
+            net_lstm,
             name='out',
             num_hidden=num_outputs)
 
-    net_lstm = mx.sym.LogisticRegressionOutput(net_lstm,
-                                               name='softmax')
+    net_lstm = mx.sym.LogisticRegressionOutput(
+            net_lstm,
+            name='softmax')
 
     mod = mx.mod.Module(net_lstm, context=mx.gpu())
 
@@ -133,7 +147,8 @@ def train_lstm(train_iter, val_iter,
              label_shapes=train_iter.provide_label)
 
     """Xavier not accepted for initialization in LSTM"""
-    mod.init_params(initializer=mx.init.Xavier())
+    mod.init_params(mx.initializer.Uniform(scale=1.0))
+
     mod.init_optimizer(optimizer='Adam',
                        optimizer_params=(('learning_rate', 0.01)))
 
@@ -149,6 +164,7 @@ def train_lstm(train_iter, val_iter,
             mod.backward()
             mod.update()
         print('Epoch %d, Training %s' % (epoch))
+
 
 
 #    mod.fit(train_data=train_iter,
